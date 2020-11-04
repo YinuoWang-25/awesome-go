c249acb994241d4e498ed628ee0738d270cbd690
YinuoWang-25 <yinuowang0825@gmail.com>
2020-11-03 21:58:33 -0800

    delete useless file

--- src/basic/L06_pointer_test.go
+++ src/basic/L06_pointer_test.go
@@ -1,1 +1,12 @@
 package basic
+
+import "testing"
+
+// 0. pointer
+// We can not do operation on pointer like C
+func TestPointer (t *testing.T) {
+	a := 2
+	var pa *int = &a
+	*pa = 3
+	t.Log(a)
+}
